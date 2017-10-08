d6e621de35e68006fd2f0bf1a5d2f7760598bdfe
Tarek Said <tarek.said.info@gmail.com>
2017-10-07 23:55:31 -0300

    Corrigindo testes de integração do user-service

--- user-service/src/main/java/br/com/tarek/login/users/config/WebSecurityConfig.java
+++ user-service/src/main/java/br/com/tarek/login/users/config/WebSecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
@@ -14,16 +13,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private AuthUserDetailsService authUserDetailsService;
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .antMatcher("/**")
-                .authorizeRequests()
-                .antMatchers("/login").permitAll()
-                .anyRequest().authenticated();
-    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
